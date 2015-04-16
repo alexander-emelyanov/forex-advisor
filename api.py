c1253f31caf7acfc04a371662e43d504e4d8313a
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-04-16 23:40:50 +0300

    First version of api under contruction

--- api.py
+++ api.py
@@ -1,12 +1,27 @@
-from app import app
+# from app import app
+from app import *
+from flask import jsonify
 from flask.ext.restful import Api, Resource
+from marshmallow import Schema, fields, ValidationError
 
 api = Api(app)
 
+# SCHEMAS
 
-class RecentSignals(Resource):
+
+class SignalSchema(Schema):
+
+    class Meta:
+        fields = ('id', 'time_of_creation', 'opening_time', 'closing_time', 'symbol_id')
+
+
+class SignalsResource(Resource):
 
     def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(RecentSignals, '/api')
+        signals = Signal.query.all()
+        serializer = SignalSchema(many=True)
+        result = serializer.dump(signals)
+        return jsonify({"signals": result.data})
+
+api.add_resource(SignalsResource, '/api')
