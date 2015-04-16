10d65892df4140ee6a7972349cef780e863a1d3e
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2015-04-17 02:55:34 +0300

    Interval for signals generator was decreased

--- task-generate.py
+++ task-generate.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 scheduler = BlockingScheduler()
 
 
-@scheduler.scheduled_job('interval', minutes=1)
+@scheduler.scheduled_job('interval', minutes=5)
 def timed_job():
     print("{0} signals generator running...".format(datetime.datetime.utcnow()))
     SignalsGenerator.run()
